4da91686080c74aaac6ce170a56465a0280c99fa
JoyFYan <JoyF.Yan@gmail.com>
2021-12-28 15:08:14 +0800

    add qlib_jqdata.py

--- qlib_jqdata.py
+++ qlib_jqdata.py
@@ -1,4 +1,6 @@
 import jqdatasdk
-username, password = '15680893289', '455000YzYabC'
-jqdatasdk.auth(username, password)
-jqdatasdk.get_price("000001.XSHE", start_date="2017-01-01", end_date="2017-12-31")
+
+
+class qlib_jqdata():
+    def __init__(self) -> None:
+        pass
